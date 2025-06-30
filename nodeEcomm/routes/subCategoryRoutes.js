cc1082469bffa4d76de4716bdbaebb4a790a88b2
Saurabhdhasmana <saurabhdhasmana2019@gmail.com>
2025-06-30 05:17:45 -0700

    add

--- nodeEcomm/routes/subCategoryRoutes.js
+++ nodeEcomm/routes/subCategoryRoutes.js
@@ -3,6 +3,8 @@ const router=express.Router();
 const {subCategoryController,getAllSubCategories,updateSubCategory,deleteSubCategory}=require("../controllers/subCategoryController");
 const {upload}=require("../middleware/multer")
 
+
+
 router.post("/subcategory", upload.single("image"), subCategoryController);
 router.get("/subcategory", getAllSubCategories);
 router.put("/subcategory/:id", upload.single("image"), updateSubCategory);
